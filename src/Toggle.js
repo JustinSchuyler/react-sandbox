eec99551e1869fc006cf3bdec69e8690700c873f
JustinSchuyler <justin.r.schuyler@gmail.com>
2017-05-03 23:29:28 -0500

    feat: add toggle component

---
+++ src/Toggle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isOn ? 'On' : 'Off'}
+      </button>
+    );
+  }
+}
+
+export default Toggle;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Timer from './Timer';
+import Toggle from './Toggle';
 import './index.css';
 
 ReactDOM.render(
   <div>
     <App />
     <Timer />
+    <Toggle />
   </div>,
   document.getElementById('root')
 );
