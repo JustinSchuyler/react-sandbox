71cb7ce0abd9366be74d439a2e08e55ad0068f82
JustinSchuyler <justin.r.schuyler@gmail.com>
2017-05-13 10:32:24 -0500

    feat: delete comment

--- src/Comments.js
+++ src/Comments.js
@@ -37,6 +37,24 @@ class Comments extends React.Component {
     });
   }
 
+  deleteComment(comment) {
+    const url = `http://localhost:8080/comments/${comment._id}`;
+    let comments = this.state.comments.slice();
+    let index = comments.indexOf(comment);
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+      comments.splice(index, 1);
+      this.setState({comments});
+    };
+    xhr.onerror = (error) => {
+      console.log(error);
+    };
+    xhr.open('DELETE', url, true);
+
+    xhr.send();
+  }
+
   render() {
     return (
       <ul className="Comments">
@@ -44,6 +62,7 @@ class Comments extends React.Component {
           <li key={comment._id}>
             <div>{(new Date(comment.addDate)).toDateString()}</div>
             <div>{comment.text}</div>
+            <button onClick={() => this.deleteComment(comment)} className="delete">X</button>
           </li>
         ))}
         {(this.state.error) ? (
