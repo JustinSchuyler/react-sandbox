e5c8fa6d3e355934d8ccf511b57264441056f252
JustinSchuyler <justin.r.schuyler@gmail.com>
2017-05-18 19:20:39 -0500

    feat(routing): added basic routing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import Calculator from './Calculator/Calculator';
 import Comments from './Comments/Comments';
 import Game from './Game/Game';
-import Timer from './Timer';
-import Toggle from './Toggle';
 import './index.css';
 
+const Home = () => (
+  <h1>React Sandbox</h1>
+);
+
 ReactDOM.render(
-  <div>
-    <App />
-    <Calculator />
-    <Timer />
-    <Comments />
-    <Game />
-    <Toggle />
-  </div>,
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/comments">Comments</Link></li>
+        <li><Link to="/game">Game</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/calculator" component={Calculator} />
+      <Route path="/comments" component={Comments} />
+      <Route path="/game" component={Game} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
